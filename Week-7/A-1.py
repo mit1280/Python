e11400c937dde1a42d0bf5d70c39e600fd527908
mit1280 <44032676+mit1280@users.noreply.github.com>
2019-03-13 20:10:27 +0530

    Update A-1.py

--- Week-7/A-1.py
+++ Week-7/A-1.py
@@ -1,1 +1,18 @@
-
+n=int(input())
+x = [[0 for i in range(n)] for j in range(n)]
+for i in range(n):
+    p=list(map(int,input().split(" ",n)))
+    for j in range(n):
+        x[i][j]=p[j]
+for i in range(n):
+    for j in range(n):
+        if i<j:
+            x[j][i]=0
+for i in range(n):
+    for j in range(n):
+      if j==n-1:
+        print(x[i][j],end='')
+      else:
+        print(x[i][j],end=' ')
+    if i<n-1:
+      print()
