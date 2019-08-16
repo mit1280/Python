0d204933b901a7f916e4b6d7af190e413d762a87
MIT PATEL <44032676+mit1280@users.noreply.github.com>
2019-08-16 09:38:56 +0530

    Update A-2.py

--- Week-3/A-2.py
+++ Week-3/A-2.py
@@ -1,3 +1,25 @@
+Given a list A of numbers, you have to print those numbers which are not multiples of 3.
+
+Input Format:
+
+The first line contains the numbers of list A separated by a space.
+
+Output Format:
+
+Print the numbers in a single line separated by a space which are not multiples of 3.
+
+Example:
+
+Input:
+
+1 2 3 4 5 6 5
+
+Output:
+
+1 2 4 5 5
+
+#code
+
 X = list(map(int, input().split(" "))) 
 Y=list()
 for i in range(len(X)):
