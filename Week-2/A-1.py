dc6f5015595c40b7f38faa33bf9ca931003db9cc
MIT PATEL <44032676+mit1280@users.noreply.github.com>
2019-08-16 09:34:39 +0530

    Update A-1.py

--- Week-2/A-1.py
+++ Week-2/A-1.py
@@ -1,3 +1,22 @@
+In this assignment, you will have to take two numbers as input and print the difference.
+
+Input Format:
+
+The first line of the input contains two numbers separated by a space.
+
+Output Format:
+
+Print the difference in single line
+
+Example:
+
+Input:
+4 2
+
+Output:
+2
+#code
+
 x, y = map(int, input().split())
 z=int(x)-int(y)
 print(z)
