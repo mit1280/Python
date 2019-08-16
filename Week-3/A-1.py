f7aaed773a1545f70f22715f99b0def0c3ce7487
MIT PATEL <44032676+mit1280@users.noreply.github.com>
2019-08-16 09:38:06 +0530

    Update A-1.py

--- Week-3/A-1.py
+++ Week-3/A-1.py
@@ -1,3 +1,25 @@
+Given a list of numbers, find maximum and minimum in this list.
+
+Input Format:
+
+The first line contains numbers separated by a space.
+
+Output Format:
+
+Print maximum and minimum separated by a space
+
+Example:
+
+Input:
+
+1 2 3 4 5
+
+Output:
+
+5 1
+
+#code
+
 X = list(map(int, input().split(" "))) 
 for i in range(len(X)):
   for j in range(0,len(X)-i-1):
